7245c53bc54ad528c46f14a107e042d1106c3dfc
fanwenke <wenfwk@163.com>
2017-01-18 18:12:50 +0800

    测试上传1

--- app/src/main/java/com/fwk/school4/ui/Jie/JieStationMapActivity.java
+++ app/src/main/java/com/fwk/school4/ui/Jie/JieStationMapActivity.java
@@ -100,7 +100,6 @@ public class JieStationMapActivity extends BasaActivity implements NetWorkListen
             adapter.setPostion(stationPosition);
             adapter.setNumberSX();
             adapter.notifyDataSetChanged();
-            LogUtils.d("aaa");
         } else {
             try {
                 StateStationBean stateStationBean = (StateStationBean) sp.queryForSharedToObject(Keyword.STATESTATIONBEAN);
